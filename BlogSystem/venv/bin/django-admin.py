ab55bb9510fd17a84f69fa60d84145b97ca0e98c
宋志 <songzhi@pinsmedical.com>
2018-08-20 17:19:57 +0800

    Project create

---
+++ BlogSystem/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zhugeyou/Desktop/MyGitHub/BlogSystem-Django/BlogSystem/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
